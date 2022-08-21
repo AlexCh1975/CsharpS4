8455c09947900b8212158a9233099faf39a9fc15
AlexCh <stovertikal@mail.ru>
2022-08-21 12:49:10 +0700

    add task 024

---
+++ task_024/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+    Задача 24: Напишите программу, которая
+    принимает на вход число (А) и выдаёт сумму чисел
+    от 1 до А.
+    7 -> 28
+    4 -> 10
+    8 -> 36
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int resultSum = GetSum(number);
+PrintSum(resultSum);
+
+int GetSum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+void PrintSum(int sum)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Сумма чисел от 1 до {number} = {sum}");
+    Console.ResetColor();
+}
